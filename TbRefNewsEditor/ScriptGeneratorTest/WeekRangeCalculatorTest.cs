3468d1575f2666039dc20b2447cc12b08a4cbd02
san-had <hadhazis@gmail.com>
2020-03-25 21:43:30 +0100

    reverted weekrangecalculator test

--- TbRefNewsEditor/ScriptGeneratorTest/WeekRangeCalculatorTest.cs
+++ TbRefNewsEditor/ScriptGeneratorTest/WeekRangeCalculatorTest.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using Moq;
 using Ninject;
 using NUnit.Framework;
@@ -15,28 +14,6 @@ namespace ScriptGeneratorTest
         private Mock<IDateTimeNowProvider> dateTimeNowProviderMock;
         private IWeekRangeCalculator weekRangeCalculator;
 
-        public static IEnumerable<TestCaseData> DateTimeNowProviderTestCases
-        {
-            get
-            {
-                yield return new TestCaseData(new Mock<IDateTimeNowProvider>()
-                    .SetupGet(x => x.Now)
-                    .Returns(new DateTime(2020, 3, 15)),
-                    "március 16 - 22")
-                    .SetName("Range inside the month");
-                yield return new TestCaseData(new Mock<IDateTimeNowProvider>()
-                     .SetupGet(x => x.Now)
-                     .Returns(new DateTime(2020, 3, 29)),
-                     "március 30 - április 5")
-                     .SetName("Range outside the month");
-                yield return new TestCaseData(new Mock<IDateTimeNowProvider>()
-                      .SetupGet(x => x.Now)
-                      .Returns(new DateTime(2020, 3, 31)),
-                      "április 1 - 7")
-                      .SetName("Range inside the month with next week new month");
-            }
-        }
-
         [OneTimeSetUp]
         public void Init()
         {
@@ -47,25 +24,12 @@ namespace ScriptGeneratorTest
             weekRangeCalculator = kernel.Get<IWeekRangeCalculator>();
         }
 
-        //[TestCase(2020, 3, 15, "március 16 - 22", TestName = "Range inside the month")]
-        //[TestCase(2020, 3, 29, "március 30 - április 5", TestName = "Range outside the month")]
-        //[TestCase(2020, 3, 31, "április 1 - 7", TestName = "Range inside the month with next week new month")]
-        //public void WeekRangeTest(int year, int month, int day, string expected)
-        //{
-        //    dateTimeNowProviderMock.SetupGet(x => x.Now).Returns(new DateTime(year, month, day));
-        //    kernel.Rebind<IDateTimeNowProvider>().ToConstant(dateTimeNowProviderMock.Object);
-
-        // string actual = weekRangeCalculator.CalculateWeekRange();
-
-        //    Assert.AreEqual(expected, actual);
-        //}
-
-        //[TestCase(2020, 3, 15, "március 16 - 22", TestName = "Range inside the month")]
-        //[TestCase(2020, 3, 29, "március 30 - április 5", TestName = "Range outside the month")]
-        //[TestCase(2020, 3, 31, "április 1 - 7", TestName = "Range inside the month with next week new month")]
-        [TestCaseSource("DateTimeNowProviderTestCases")]
-        public void WeekRangeTest(Mock<IDateTimeNowProvider> dateTimeNowProviderMock, string expected)
+        [TestCase(2020, 3, 15, "március 16 - 22", TestName = "Range inside the month")]
+        [TestCase(2020, 3, 29, "március 30 - április 5", TestName = "Range outside the month")]
+        [TestCase(2020, 3, 31, "április 1 - 7", TestName = "Range inside the month with next week new month")]
+        public void WeekRangeTest(int year, int month, int day, string expected)
         {
+            dateTimeNowProviderMock.SetupGet(x => x.Now).Returns(new DateTime(year, month, day));
             kernel.Rebind<IDateTimeNowProvider>().ToConstant(dateTimeNowProviderMock.Object);
 
             string actual = weekRangeCalculator.CalculateWeekRange();
